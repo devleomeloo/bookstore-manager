b1b0089db126b0b31b0df5f0973c873945443ff1
Leonardo Melo de Jesus (P) <X276075@prservicos.com.br>
2021-10-14 11:56:39 -0300

    Initial commit

---
+++ src/main/java/com/leonardo/bookstoremanager/controller/BookController.java
@@ -0,0 +1,22 @@
+package com.leonardo.bookstoremanager.controller;
+
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+public class BookController {
+
+    @ApiOperation(value = "API Example")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Success")
+    })
+    @GetMapping
+    public String test(){
+        return "Test";
+    }
+}
