ca8a54b9e4edef16badb9c954e5a0931e79f30a9
Leonardo Melo de Jesus (P) <X276075@prservicos.com.br>
2021-10-29 17:19:03 -0300

    created unit test to check the findAll method

--- src/test/java/com/leonardo/bookstoremanager/controller/AuthorControllerTest.java
+++ src/test/java/com/leonardo/bookstoremanager/controller/AuthorControllerTest.java
@@ -4,7 +4,6 @@ import com.leonardo.bookstoremanager.author.builder.AuthorDTOBuilder;
 import com.leonardo.bookstoremanager.dto.AuthorDTO;
 import com.leonardo.bookstoremanager.service.AuthorService;
 import com.leonardo.bookstoremanager.utils.JsonConversionUtils;
-import org.hamcrest.core.Is;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -17,6 +16,9 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.servlet.view.json.MappingJackson2JsonView;
 
+import java.util.Collections;
+
+import static org.hamcrest.core.Is.is;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -59,9 +61,9 @@ public class AuthorControllerTest {
         .contentType(MediaType.APPLICATION_JSON)
         .content(JsonConversionUtils.asJsonString(expectedCreatedAuthorDTO)))
                 .andExpect(status().isCreated())
-                .andExpect(jsonPath("$.id", Is.is(expectedCreatedAuthorDTO.getId().intValue())))
-                .andExpect(jsonPath("$.name", Is.is(expectedCreatedAuthorDTO.getName())))
-                .andExpect(jsonPath("$.age", Is.is(expectedCreatedAuthorDTO.getAge())));
+                .andExpect(jsonPath("$.id", is(expectedCreatedAuthorDTO.getId().intValue())))
+                .andExpect(jsonPath("$.name", is(expectedCreatedAuthorDTO.getName())))
+                .andExpect(jsonPath("$.age", is(expectedCreatedAuthorDTO.getAge())));
     }
 
     @Test
@@ -87,8 +89,23 @@ public class AuthorControllerTest {
         mockMvc.perform(get(AUTHOR_API_URL_PATH + "/" + expectedFoundAuthorDTO.getId())
                 .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$.id", Is.is(expectedFoundAuthorDTO.getId().intValue())))
-                .andExpect(jsonPath("$.name", Is.is(expectedFoundAuthorDTO.getName())))
-                .andExpect(jsonPath("$.age", Is.is(expectedFoundAuthorDTO.getAge())));
+                .andExpect(jsonPath("$.id", is(expectedFoundAuthorDTO.getId().intValue())))
+                .andExpect(jsonPath("$.name", is(expectedFoundAuthorDTO.getName())))
+                .andExpect(jsonPath("$.age", is(expectedFoundAuthorDTO.getAge())));
+    }
+
+    @Test
+    void whenGETListIsCalledThenStatusOkShouldBeReturned() throws Exception {
+
+        AuthorDTO expectedFoundAuthorDTO = authorDTOBuilder.buildAuthorDTO();
+
+        when(authorService.findAll()).thenReturn(Collections.singletonList(expectedFoundAuthorDTO));
+
+        mockMvc.perform(get(AUTHOR_API_URL_PATH)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].id", is(expectedFoundAuthorDTO.getId().intValue())))
+                .andExpect(jsonPath("$[0].name", is(expectedFoundAuthorDTO.getName())))
+                .andExpect(jsonPath("$[0].age", is(expectedFoundAuthorDTO.getAge())));
     }
 }
