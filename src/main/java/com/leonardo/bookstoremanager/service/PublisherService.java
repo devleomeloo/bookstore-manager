6f6456dab9d949b077bad70bdb1fca6a6f9171f6
Leonardo Melo de Jesus (P) <X276075@prservicos.com.br>
2021-11-05 11:51:04 -0300

    Created publisher service

---
+++ src/main/java/com/leonardo/bookstoremanager/service/PublisherService.java
@@ -0,0 +1,19 @@
+package com.leonardo.bookstoremanager.service;
+
+import com.leonardo.bookstoremanager.mapper.PublisherMapper;
+import com.leonardo.bookstoremanager.repository.PublisherRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PublisherService {
+
+    private final static PublisherMapper publisherMapper = PublisherMapper.INSTANCE;
+
+    private PublisherRepository publisherRepository;
+
+    @Autowired
+    public PublisherService(PublisherRepository publisherRepository) {
+        this.publisherRepository = publisherRepository;
+    }
+}
