263ee3d6aad2288f4a25e6e3617a758b795e2075
devleomeloo <leomelojesus@gmail.com>
2022-05-23 15:44:50 -0300

    Adjusted Swagger URL

--- src/main/java/com/leonardo/bookstoremanager/config/SwaggerConfig.java
+++ src/main/java/com/leonardo/bookstoremanager/config/SwaggerConfig.java
@@ -16,7 +16,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 public class SwaggerConfig {
 
     private static final String NAME = "Leo Melo";
-    private static final String URL = "www.corinthians.com.br";
+    private static final String URL = "https://github.com/devleomeloo/bookstore-manager";
     private static final String EMAIL = "lmelojesus@live.com";
     private static final String BASE_PACKAGE = "com.leonardo.bookstoremanager";
     private static final String VERSION = "1.0.0";
